50d2c994236092603a0caf9cd11e37204b3ab6e8
WalukaUI <cwaluka@yahoo.com>
2021-07-02 22:57:20 -0700

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    if (array.size==1)
+        return array[0]
+    elsif (array.size==2)
+        return array.join(" and ")
+    elsif (array.size==3)
+        last_part=array[-1].insert(0, "and ")
+        return  "#{array[0]}, #{array[1]}, #{last_part}"
+    elsif (array.size>3)
+        rnew_1=array[-1].insert(0, "and ")
+        new_2=array[0,array.size-1]
+        last=new_2 << rnew_1
+        last.join(", ")
+    end
+      
+    
 end
